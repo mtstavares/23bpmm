12380bbd2b12195b3150c9c18ef18e70cbe5ca72
Matheus Tavares <130933980+mtstavares@users.noreply.github.com>
2024-03-27 17:06:59 -0300

    Add files via upload
    Commit 27MAR24

---
+++ script.js
@@ -0,0 +1,51 @@
+const meses = {
+    janeiro: {
+        foto: "PMMes/janeiro/pm-mes.jpg",
+        sintese: "Pelos relevantes trabalhos prestados nesta OPM, realizando suas funções com eficiência e maestria, sempre demonstrando amplo interesse no desenvolvimento de suas missões. Além disso, vem obtendo grande destaque nesta Cia há vários meses. Em 06JAN24, quando em patrulhamento com motocicleta, por volta das 18h45min, visualizado uma motocicleta transitando vagarosamente entre os carros e observando o interior deles, enquanto falava ao celular. Dado ordem de parada para abordagem pela Rua Pedroso Alvarenga e em busca pessoal foi localizado em sua cintura um revolver belga Brevete Nagant M1910 calibre .32 com numeração e uma munição picotada. De imediato foi dado voz de prisão ao indivíduo, o qual confessou a equipe que estava em ligação com seus comparsas para repassar o armamento para outro indivíduo realizar o roubo a relógio, mencionando ainda que haviam mais dois indivíduos em uma SUV vermelha que indicariam o relógio a ser roubado. Diante dos fatos ocorrência encaminhada ao 14º DP, onde a autoridade de plantão elaborou BOPC de Porte Ilegal de arma de fogo, além da apreensão da motocicleta. No dia 10JAN24, por volta das 12h00min, quando em patrulhamento com motocicleta, se deparado com um indivíduo na condução de uma motocicleta com uma mochila de aplicativo de entrega pela Ponte Eusébio Matoso, que ao visualizar a equipe diminuiu a velocidade em que transitava para não a ultrapassar, porém não foi possível devido a fluidez do trânsito, sendo visualizado um volume na cintura do condutor. De imediato a equipe acompanhou a motocicleta, sendo dada a ordem de parada em um local mais seguro, sendo realizada a abordagem na Praça Eugene Boudin, acesso da Ponte Eusébio Matoso para a Marginal Pinheiros sentido Rodovia Pres. Castelo Branco, onde em busca pessoal foi localizado na cintura do abordado um revolver calibre .32 municiado com 06 (seis) munições intactas e na busca veicular foi verificado que o chassi da motocicleta estava divergente com a placa ostentada. Ao realizar consulta junto ao COPOM foi verificado que o indivíduo era menor de idade e em conversa informal o sindicado confessou que pegou a motocicleta em um posto de gasolina no município de Taboão da Serra, estando a chave e o armamento no interior da mochila de entrega e ganharia uma quantia de R$ 300,00 para entregar o armamento a outra pessoa nas proximidades do Shopping Iguatemi, e que desconhece o dono da motocicleta. Diante dos fatos ocorrência encaminhada ao 14º DP, onde a autoridade de plantão elaborou BOPC de Porte Ilegal de arma de fogo/ Adulteração de sinal identificador de veículo automotor/ dirigir sem habilitação, além da apreensão da motocicleta para perícia. Em 12JAN24, durante patrulhamento com motocicleta pela Ponte Cidade Jardim, foi visualizado no contra fluxo da via duas motocicletas, trafegando juntas, cada moto com um indivíduo, sendo que o primeiro utilizava uma mochila de aplicativo de entrega e o segundo foi visto mexendo na região da cintura. De imediato a equipe realizou a abordagem na segunda motocicleta, não sendo possível abordar a primeira. Em busca pessoal foi localizado dois aparelhos celulares e após consulta de IMEI, ambos acusaram ser produto de crime. Na consulta veicular foi constatado que a placa e o chassi conferiam, porém a numeração de motor constava de outra motocicleta, sendo esta produto de furto de 30NOV23. Ocorrência encaminhada ao 14º DP, onde autoridade de plantão elaborou BOPC de Receptação/ Adulteração de Sinal Identificador de veículo/ Dirigir sem habilitação/ Apreensão de objeto, além da apreensão da motocicleta para perícia. A pronta atuação do Sd PM Bertole demonstra alto tirocínio policial, destreza, empenho, motivação e compromisso com a causa pública, além de demonstrar profissionalismo e maestria na execução de suas funções, exemplo a ser seguido por seus companheiros, recebendo diversos elogios por parte dos seus comandantes. O policial exerce diariamente papel de destaque em seus serviços prestados a sociedade, demonstrando proatividade, além de prevenir dois roubos iminentes, retirou dois armamentos que circulavam pelas ruas e protegeu pessoas de serem futuras vítimas da criminalidade, sendo assim, estas ações enaltecem o nome da Polícia Militar do Estado de São Paulo perante nossa sociedade, razão pela qual recebe este reconhecimento"
+    },
+    fevereiro: {
+        foto: "PMMes/fevereiro/pm-mes.png",
+        sintese: "Indicado como Policial do Mês de fevereiro de 2024 da 3ª Cia do 23º BPM/M, por ter se destacado em atendimento de ocorrências na área territorial desta subunidade e além de suas missões, estar sempre pronto a ajudar e apoiar os policiais de sua equipe. Profissional ímpar, experiente, extremamente interessado e competente, cumpridor de suas obrigações e deveres, revelando-se uma pessoa digna do respeito dos seus superiores, pares e subordinados, por sua brilhante atuação e desempenho na função de Comando de Grupo Patrulha da equipe ‘Alfa. Ganhando destaque pela ocorrência de 20FEV24, onde durante patrulhamento pela comunidade do Sapé, recebeu ligação do Cb PM Daniel, que estava de folga, e repassou informações sobre o paradeiro de um indivíduo envolvido na prática de roubos a veículos efetuados no período da manhã dessa mesma data. De posse das informações e com o apoio das demais viaturas da 3º cia, realizaram incursão pelas vielas da referida comunidade supracitada, e pela rua Waldemar Roberto, numeral 420, local de possível residência do indivíduo suspeito, as equipes realizaram contato prévio com uma moradora, que em seguida permitiu a entrada das equipes para averiguação, e informou residir no local com o seu filho, este que se encontrava dormindo, uma vez que havia acabado de chegar da rua. De pronto, as equipes realizaram averiguação no interior da residência e a abordagem ao indivíduo, que durante a entrevista confessou ter realizado os crimes, além disso, entregou aos policiais os objetos que havia subtraído na prática do ilícito. Diante dos fatos, o averiguado e os objetos foram encaminhados ao 14º distrito policial, onde compareceram cinco vítimas do crime, que realizaram o reconhecimento dos objetos e do detido como autor do ilícito, cabendo a autoridade de plantão ratificar a ordem de prisão e elaborar boletim de ocorrência da Polícia Civil de natureza Roubo consumado. O militar em destaque demonstra cotidianamente sua vocação profissional nas atividades e seu comprometimento no serviço operacional, sempre se dedicando de maneira excepcional. Deixando evidente o profissionalismo, dedicação e atenção, sendo exemplo a seus pares e subordinados, motivos pelos quais se faz merecedor do destaque de policial do mês de fevereiro."
+    },
+
+};
+
+const cards = document.querySelectorAll(".card");
+const sintese = document.querySelector("#sintese-texto");
+const leiaMais = document.querySelector('#leia-mais');
+const fotoPolicial = document.querySelector("#foto-policial img");
+
+
+function atualizarInformacoesDoMes(mes) {
+    if (meses.hasOwnProperty(mes)) {
+        const fotoDoMes = meses[mes].foto;
+        const sinteseDoMes = meses[mes].sintese;
+
+        fotoPolicial.src = fotoDoMes;
+        sintese.innerHTML = sinteseDoMes;
+
+        if (sintese.scrollHeight > sintese.clientHeight) {
+            leiaMais.style.display = "block";
+        } else {
+            leiaMais.style.display = "none";
+            sintese.style.overflow = "auto"
+        }
+    } else {
+        alert(`Ainda não há informações para o mês de ${mes}.`);
+        
+    }
+}
+
+cards.forEach(card => {
+    card.addEventListener("click", () => {
+        leiaMais.style.display = "block";
+        const mesClicado = card.querySelector("h4").innerText.toLowerCase();
+        atualizarInformacoesDoMes(mesClicado);
+    });
+});
+
+leiaMais.addEventListener("click", () => {
+    leiaMais.style.display = "none";
+    sintese.style.overflowY = "scroll"
+});
+
